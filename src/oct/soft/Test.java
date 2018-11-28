1e43bc45a4797055d594e405e57d0d2e8ceabe69
Octavian Santau <octavian.santau@palstor.ro>
2018-11-28 15:03:15 +0200

    Add schema to db.

--- src/oct/soft/Test.java
+++ src/oct/soft/Test.java
@@ -26,8 +26,8 @@ public class Test {
         ByteArrayInputStream bais = new ByteArrayInputStream(content.getBytes("UTF-8"));
          BaseObject baseObject = mapper.readValue(bais, BaseObject.class); 
         CompanyInfoDao companyInfoDao = new CompanyInfoDao();
-//        companyInfoDao.persist(baseObject.getFound());
-        System.out.println(companyInfoDao.findAll().size());
+        companyInfoDao.persist(baseObject.getFound());
+//        System.out.println(companyInfoDao.findAll().size());
         System.exit(0);
     }
 }
