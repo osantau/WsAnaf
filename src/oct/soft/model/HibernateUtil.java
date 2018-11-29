2c0f401df591c4694eff74fb8feff11c04a90ee0
Octavian Santau <octavian.santau@palstor.ro>
2018-11-29 14:11:21 +0200

    Remove usage of hibernate xml configuration. Add customize configuration for database. For now h2, mysql, postgresql. Add configuration files.

--- src/oct/soft/model/HibernateUtil.java
+++ src/oct/soft/model/HibernateUtil.java
@@ -5,6 +5,9 @@
  */
 package oct.soft.model;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Properties;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -19,15 +22,25 @@ import org.hibernate.service.ServiceRegistry;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
-
+    public static void main(String[] args) throws Exception{
+    Properties properties = new Properties();           
+            properties.load(new FileReader("./config/app.properties"));
+        System.out.println(properties.get("db.type"));        
+    }
     static {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
+            Properties appProps = new Properties();           
+            appProps.load(new FileReader("./config/app.properties"));
+            
+            Properties properties = new Properties();           
+            properties.load(new FileInputStream("./config/hibernate-"+appProps.getProperty("db.type")+".properties"));
             Configuration configuration = new Configuration();
-            configuration.configure("hibernate.cfg.xml");
+            configuration.setProperties(properties);         
+            configuration.addAnnotatedClass(CompanyInfo.class);
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);            
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
